6445426a9607efc0f9c3cc51f1965d4649b3f313
Leonardo Martins <leonardo.f.martins@icloud.com>
2022-10-20 20:21:31 -0300

    update files

---
+++ modulo-01/semana-03/aula-03/script.js
@@ -0,0 +1,7 @@
+const array = ['item1'];
+
+array[0] = 'vazia';
+
+array.push('item2');
+
+console.log(array);
---
+++ modulo-01/semana-03/aula-04/script.js
@@ -0,0 +1,32 @@
+// Rest
+
+// function sumParameters(item1, ...numbers) {
+//     console.log({numbers});
+//
+//     let accumulator = 0;
+//
+//     numbers.forEach(value => accumulator += value);
+//
+//     return accumulator;
+// }
+//
+// console.log(sumParameters(1, 2, 3, 4, 5));
+
+
+// Spread
+
+// const arrayA = [1, 2, 3, 4];
+// const arrayB = [55, 66, 77, 88, ...arrayA];
+// const arrayC = [...arrayA,...arrayB];
+//
+// console.log(arrayA);
+// console.log(arrayB);
+// console.log(arrayC);
+
+// Spread Objects
+
+// const objectA = {a: 1, b: 2, c: 3};
+// const objectB = {a: 55, b: 66, c: 77};
+// const objectC = {...objectA, ...objectB};
+//
+// console.log(objectC);
