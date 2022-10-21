8248546f26f1981df131d6c773432f7d41708507
Leonardo Martins <leonardo.f.martins@icloud.com>
2022-10-20 22:35:48 -0300

    atualiza exercicios aula 04

--- modulo-01/semana-03/aula-04/script.js
+++ modulo-01/semana-03/aula-04/script.js
@@ -30,3 +30,86 @@
 // const objectC = {...objectA, ...objectB};
 //
 // console.log(objectC);
+
+// Destructuring Vetor
+
+// function sumDestructuringArray(array) {
+//     const [a, b] = array1;
+//
+//     console.log({array1});
+//
+//     let accumulator = 0;
+//
+//     array.forEach(value => accumulator += value);
+//
+//     return accumulator;
+// }
+//
+// console.log(sumDestructuringArray([1, 2, 3, 4]));
+
+
+// function procuraTermo(params) {
+//   const { texto, termo, ...resto} = params;
+//
+//   return texto.includes(params);
+// }
+//
+// console.log(
+//   procuraTermo('O rato roeu a roupa do rei de Roma.', )
+// )
+
+// Optional chaining
+
+// const object = {a: 1};
+//
+// console.log(object.a);
+// console.log(object?.b);
+// console.log(object.b?.a);
+
+// Brincando com destructuring e um pouco de classes e objetos
+
+class Person {
+  constructor(name, age, married) {
+    this.name = name;
+    this.age = age;
+    this.married = married;
+  }
+};
+
+const person1 = new Person();
+
+person1.name = 'Leonardo';
+person1.age = 38;
+person1.married = true;
+
+const person2 = new Person();
+
+person2.name = 'Rosa';
+person2.age = 72;
+person2.married = false;
+
+
+// const person1 = { name: 'Leonardo', age: 38, married: true };
+// const person2 = { name: 'Sandra', age: 69, married: true };
+// const person3 = { name: }
+
+const {name: personName, ...rest} = person2;
+
+const demaisDados = {...rest};
+
+let married;
+
+demaisDados.married ? married = 'sim' : married = 'não';
+
+
+console.log(`O nome da pessoa é ${personName}. Seus demais dados são:\nIdade: ${demaisDados.age}\nCasado: ${married}`);
+
+// console.log(`O cliente é ${personName}`);
+
+// console.log(`O cliente ${personName} tem ${personAge} anos.`);
+
+// const cars = ['fusca', 'corcel', 'opala'];
+//
+// const [,,car3] = cars;
+//
+// console.log(car3);
