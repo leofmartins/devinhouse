8d58d5dfd2a69343d3eff8bc6411b59190bf46dd
Leonardo da Fonseca Martins <leonardo.f.martins@icloud.com>
2022-11-02 20:56:30 -0300

    concluir exercicio 07 semana 03 modulo 01

--- modulo-01/semana-03/exercicios/ex-07/app.js
+++ modulo-01/semana-03/exercicios/ex-07/app.js
@@ -4,16 +4,21 @@ const pessoa = {
   profissao: 'matemática'
 };
 
-function mensagem(objeto) {
-  const { nome, idade, profissao } = objeto;
-  return `Esta é ${nome}, tem ${idade} anos e é ${profissao}.`;
-}
+// function mensagem(objeto) {
+//   const { nome, idade, profissao } = objeto;
+//   return `Esta é ${nome}, tem ${idade} anos e é ${profissao}.`;
+// }
 
 // Extra 1:
 
-// function mensagem(objeto) {
-//
+// function mensagem({ nome, idade, profissao }) {
+//   return `Esta é ${nome}, tem ${idade} anos e é ${profissao}.`;
 // }
+
+// Extra 2
+
+const mensagem = ({ nome, idade, profissao }) => `Esta é ${nome}, tem ${idade} anos e é ${profissao}.`;
+
 //
 //
 console.log(mensagem(pessoa));
