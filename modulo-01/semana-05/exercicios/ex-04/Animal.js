53afd01e18410248f0e5323e34ef9db19e2fe0bf
Leonardo da Fonseca Martins <leonardo.f.martins@icloud.com>
2022-11-04 19:56:20 -0300

    resolve ex-04 s-05 m-01

---
+++ modulo-01/semana-05/exercicios/ex-04/Animal.js
@@ -0,0 +1,25 @@
+export default class Animal {
+  nome
+  idade
+  som
+
+  constructor(nome, idade, som) {
+    this.nome = nome;
+    this.idade = idade;
+    this.som = som;
+  }
+}
+
+export class Cachorro extends Animal {
+  constructor(nome, idade) {
+    super(nome, idade);
+    this.som = 'latido';
+  }
+}
+
+export class Gato extends Animal {
+  constructor(nome, idade) {
+    super(nome, idade);
+    this.som = 'miado';
+  }
+}
---
+++ modulo-01/semana-05/exercicios/ex-04/app.js
@@ -0,0 +1,6 @@
+import {Cachorro, Gato} from "./Animal.js";
+
+const snoopy = new Cachorro('snoopy', 10);
+console.log( snoopy.som );   // "latido"
+const frajola = new Gato('frajola', 8);
+console.log( frajola.som );   // "miado"
