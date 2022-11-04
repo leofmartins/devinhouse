19a12d92baa4efc2d3a1050433057dc94d771f6f
Leonardo da Fonseca Martins <leonardo.f.martins@icloud.com>
2022-11-04 19:19:36 -0300

    refatora nome da variável para igualar a enunciado

--- modulo-01/semana-05/exercicios/ex-01/app.js
+++ modulo-01/semana-05/exercicios/ex-01/app.js
@@ -1,11 +1,11 @@
 import {Funcionario} from "./Funcionario.js";
 
-const funcionario1 = new Funcionario('123.456.789-00', 'Leonardo da Fonseca Martins', 2500);
+const ada = new Funcionario('123.456.789-00', 'Leonardo da Fonseca Martins', 2500);
 
-console.log(funcionario1.salario);
+console.log(ada.salario);
 
-funcionario1.promover(10);
+ada.promover(10);
 
-console.log(funcionario1.salario)
+console.log(ada.salario)
 
 // Falta implementar extra 02
