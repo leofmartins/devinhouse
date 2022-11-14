6efdf8298fa63c669d55b9b9932ac398efc96384
Leonardo Martins <leonardo.f.martins@icloud.com>
2022-11-14 00:42:45 -0300

    conclui ex-08 s-06 m-01

--- modulo-01/semana-06/exercicios/ex-08/script.js
+++ modulo-01/semana-06/exercicios/ex-08/script.js
@@ -1,21 +1,61 @@
-const list = document.getElementById('list');
+const list = document.querySelector('#list');
+
+const createElement = (type, contents) => {
+  const element = document.createElement(type);
+  element.innerText = contents;
+  return element;
+}
 
 async function getUsers() {
   let url = 'https://randomuser.me/api/?results=4&nat=BR';
   try {
-    let result = fetch(url);
-    return (await result).json();
+    let result = await fetch(url);
+    return await result.json();
   } catch (e) {
     console.log(e);
   }
 }
 
-const users = await getUsers();
+async function renderUsers() {
+  let object = await getUsers();
+  console.log(object.results);
 
-const results = users.results;
+  object.results.forEach(person => {
+    const li = document.createElement('li');
+    const container = document.createElement('div');
+    const info = document.createElement('div');
 
-console.log(results);
+    container.classList.add('container');
+    info.classList.add('info');
 
-results.forEach( item => {
-  console.log(item);
-} );
+    let name = createElement(
+      'p',
+      `${person.name.title} ${person.name.first} ${person.name.last}`
+    );
+
+    let email = createElement(
+      'p',
+      `${person.email}`
+    );
+
+    let adress = createElement(
+      'p',
+      `${person.location.street.name} ${person.location.street.number} ${person.location.city} ${person.location.state} ${person.location.country}`
+    )
+
+    let photo = document.createElement('img');
+
+    photo.src = person.picture.large;
+
+    info.appendChild(name);
+    info.appendChild(email);
+    info.appendChild(adress);
+    container.appendChild(photo);
+    container.appendChild(info);
+    li.appendChild(container);
+    list.appendChild(li);
+  })
+
+}
+
+renderUsers();
