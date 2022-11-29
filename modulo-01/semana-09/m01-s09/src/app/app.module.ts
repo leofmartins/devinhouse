a2c4d2109a7abff29c8713df66419e09f87b60dd
Leonardo Martins <leonardo.f.martins@icloud.com>
2022-11-29 19:59:46 -0300

    anotacões aula-02

--- modulo-01/semana-09/m01-s09/src/app/app.module.ts
+++ modulo-01/semana-09/m01-s09/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FormComponent } from './form/form.component';
+import { HomeImageComponent } from './home-image/home-image.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FormComponent,
+    HomeImageComponent
   ],
   imports: [
     BrowserModule
