433fa45f1ddb983d9a4cde2845e94107a4c13d07
Leonardo da Fonseca Martins <leonardo.f.martins@icloud.com>
2022-12-05 20:40:13 -0300

    resolve ex 07

--- modulo-01/semana-08/exercicios/src/app/notification-list/notification-list.component.ts
+++ modulo-01/semana-08/exercicios/src/app/notification-list/notification-list.component.ts
@@ -2,7 +2,31 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-notification-list',
-  templateUrl: './notification-list.component.html',
+  template: `<ul class="list-group">
+  <li class="list-group-item">
+    <h4>Lorem ipsum</h4>
+    <p>Excepteur sint occaecat cupidatat</p>
+    <p>
+      29 min ago
+    </p>
+  </li>
+
+  <li class="list-group-item">
+    <h4>Lorem ipsum</h4>
+    <p>Excepteur sint occaecat cupidatat</p>
+    <p>
+      1 day ago
+    </p>
+  </li>
+
+  <li class="list-group-item">
+    <h4>Lorem ipsum</h4>
+    <p>Excepteur sint occaecat cupidatat</p>
+    <p>
+      2 days ago
+    </p>
+  </li>
+</ul>`,
   styleUrls: ['./notification-list.component.css']
 })
 export class NotificationListComponent {
---
+++ modulo-01/semana-08/exercicios/src/notifification-items.ts
@@ -0,0 +1,19 @@
+export const notifificationItems = [
+  {
+    title: "Lorem ipsum",
+    subtitle: "Excepteur sint occaecat cupidatat",
+    date: "29 min ago"
+  },
+
+  {
+    title: "Lorem ipsum",
+    subtitle: "Excepteur sint occaecat cupidatat",
+    date: "29 min ago"
+  },
+
+  {
+    title: "Lorem ipsum",
+    subtitle: "Excepteur sint occaecat cupidatat",
+    date: "29 min ago"
+  },
+];
