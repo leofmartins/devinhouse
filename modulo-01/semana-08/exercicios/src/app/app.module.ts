1be6b78db8708f15c30619cb32d7bc1fd14046c7
Leonardo da Fonseca Martins <leonardo.f.martins@icloud.com>
2022-12-10 19:09:26 -0300

    resolve até o ex07 s10

--- modulo-01/semana-08/exercicios/src/app/app.module.ts
+++ modulo-01/semana-08/exercicios/src/app/app.module.ts
@@ -5,10 +5,40 @@ import { AppComponent } from './app.component';
 import { NotificationListComponent } from './notification-list/notification-list.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import {FormsModule} from "@angular/forms";
+import { FormsModule } from "@angular/forms";
 import { FiltroComponent } from './filtro/filtro.component';
 import { HomeComponent } from './pages/home/home.component';
 import { SobreComponent } from './pages/sobre/sobre.component';
+import { RouterModule, Routes } from "@angular/router";
+import * as path from "path";
+
+const childRoutes: Routes = [
+  {
+    path: 'todos',
+    component: NotificationListComponent
+  },  {
+    path: 'lidos',
+    component: NotificationListComponent
+  },  {
+    path: 'nao-lidos',
+    component: NotificationListComponent
+  }
+]
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },{
+    path: 'home',
+    component: HomeComponent
+  },{
+    path: 'sobre',
+    component: SobreComponent,
+    children: childRoutes
+  },
+]
 
 @NgModule({
   declarations: [
@@ -22,7 +52,8 @@ import { SobreComponent } from './pages/sobre/sobre.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
