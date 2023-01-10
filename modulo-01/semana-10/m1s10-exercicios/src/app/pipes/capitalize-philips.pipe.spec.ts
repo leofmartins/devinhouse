4ecd482abb10f9b0053dd46cac90ec29e38a7c00
Leonardo da Fonseca Martins <leonardo.f.martins@icloud.com>
2023-01-09 22:06:40 -0300

    corrige localização dos diretórios

---
+++ modulo-01/semana-10/m1s10-exercicios/src/app/pipes/capitalize-philips.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { CapitalizePhilipsPipe } from './capitalize-philips.pipe';
+
+describe('CapitalizePhilipsPipe', () => {
+  it('create an instance', () => {
+    const pipe = new CapitalizePhilipsPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
