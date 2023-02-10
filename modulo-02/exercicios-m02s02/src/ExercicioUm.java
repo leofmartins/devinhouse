1f8978fd80863e438f711ebcd4c0cadba88031ab
Leonrado da Fonseca Martins <leonardo.f.martins@icloud.com>
2023-02-10 16:25:46 -0300

    Resolve ex01s02m02

---
+++ modulo-02/exercicios-m02s02/src/ExercicioUm.java
@@ -0,0 +1,17 @@
+public class ExercicioUm {
+    public static void main(String[] args) {
+        // Implemente um código copiando o array abaixo,
+        // e depois imprima no console o valor dos seguintes elementos:
+
+        int[] arr = { 56, 57, 58, 67, 99, 12, 11, 88 };
+
+        // a) Imprima o valor do primeiro elemento;
+        System.out.println("Primeiro elemento da array: " + arr[0]);
+
+        // b) Imprima o valor do último elemento;
+        System.out.println("Último elemento da array: " + arr[arr.length - 1]);
+
+        // c) Imprima o valor do terceiro elemento;
+        System.out.println("Terceiro elemento da array: " + arr[2]);
+    }
+}
