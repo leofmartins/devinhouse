47fb3d9953673759ba1fb0a6de0ffd66fc147827
Leonardo da Fonseca Martins <leonardo.f.martins@icloud.com>
2023-02-20 15:17:47 -0300

    Resolve ex02s03m02

--- modulo-02/semana-03/exercicios-m02s03/src/Clinica.java
+++ modulo-02/semana-03/exercicios-m02s03/src/Clinica.java
@@ -1,7 +1,30 @@
+import java.sql.SQLOutput;
+
 class Clinica {
     String nomeDono;
     String nomeClinica;
     String cnpj;
     String endereco;
     int codigoClinica;
+
+    static double calculaIMC(double peso, double altura) {
+        double imc = peso / (altura * altura);
+        String resultado;
+
+        if (imc < 18.5) {
+            resultado = "Magreza";
+        } else if (imc < 25) {
+            resultado = "Normal";
+        } else if (imc < 30) {
+            resultado = "Sobrepeso";
+        } else {
+            resultado = "Obesidade";
+        }
+
+        System.out.println("Resultado: ");
+        System.out.print("IMC = " + imc);
+        System.out.println(resultado);
+
+        return imc;
+    }
 }
