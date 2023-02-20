860e1de489ebc28328793ffcc81d4c8e1b6b3975
Leonardo da Fonseca Martins <leonardo.f.martins@icloud.com>
2023-02-20 15:25:02 -0300

    Resolve ex04s03m02

--- modulo-02/semana-03/exercicios-m02s03/src/Cliente.java
+++ modulo-02/semana-03/exercicios-m02s03/src/Cliente.java
@@ -2,4 +2,28 @@ class Cliente {
     double peso;
     double altura;
     int idade;
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
 }
--- modulo-02/semana-03/exercicios-m02s03/src/Clinica.java
+++ modulo-02/semana-03/exercicios-m02s03/src/Clinica.java
@@ -10,6 +10,58 @@ class Clinica {
 
     List<Cliente> clientes;
 
+    public String getNomeDono() {
+        return nomeDono;
+    }
+
+    public void setNomeDono(String nomeDono) {
+        this.nomeDono = nomeDono;
+    }
+
+    public String getNomeClinica() {
+        return nomeClinica;
+    }
+
+    public void setNomeClinica(String nomeClinica) {
+        this.nomeClinica = nomeClinica;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public int getCodigoClinica() {
+        return codigoClinica;
+    }
+
+    public void setCodigoClinica(int codigoClinica) {
+        this.codigoClinica = codigoClinica;
+    }
+
+    public List<Cliente> getClientes() {
+        return clientes;
+    }
+
+    public void setClientes(List<Cliente> clientes) {
+        this.clientes = clientes;
+    }
+
+    public void addCliente(Cliente cliente) {
+        clientes.add(cliente);
+    }
+
     static double calculaIMC(double peso, double altura) {
         double imc = peso / (altura * altura);
         String resultado;
